7e894634a0ea74e124161cdba7a210f575ab1728
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-02-08 18:37:18 +0000

    Extracted date from the  datepicker

--- assets/js/mars-rovers.js
+++ assets/js/mars-rovers.js
@@ -1,1 +1,29 @@
 nasaApikey = "zhH0KJERQo5hx19MxD3hGmt6jiaOqgfd8bmoWQPd";
+
+let perseveranceEl = $("#Perseverance");
+let marsImagesEl = $("#mars-images");
+
+
+perseveranceEl.on("click", function (event) {
+    event.preventDefault();
+
+  $(".container-fluid").empty();
+    let datePickerEl = $("<div>");
+    let datePickerText = $("<p>").text('Choose a date to see images taken by Perseverance on this date');
+    datePickerEl.append(datePickerText).addClass('date-label');
+
+    let datePickerDateEl = $("<input>").attr("id", "perseverance-images").attr("type", "text").datepicker();
+    datePickerEl.append(datePickerDateEl);
+    
+
+
+    let date = $("#perseverance-images").val();
+    console.log(moment(date).format("DD/MM/YYYY"));
+
+    marsImagesEl.append(datePickerEl);
+  })
+
+
+ 
+
+ 
