cc3436514c738d15a8de4a0782b790d51e4a0045
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-02-09 23:38:43 +0000

    Added carousel of mars images

--- assets/js/mars-rovers.js
+++ assets/js/mars-rovers.js
@@ -2,75 +2,30 @@ nasaApikey = "zhH0KJERQo5hx19MxD3hGmt6jiaOqgfd8bmoWQPd";
 let imageEl = $("#Perseverance");
 let titleEl = $("#Perseverance-rover");
 let marsImagesEl = $("#mars-images");
+let datepickerEl =  $("#datepicker");
+let datepickerTextEl =  $("#choose-date");
 let selectedImages = [];
+
 
 imageEl.on("click", function (event) {
   event.preventDefault();
   
-
   $(".container-fluid").empty();
-  marsImagesEl.removeClass("hide");
+  datepickerEl.removeClass("hide");
+  datepickerTextEl.removeClass("hide");
 
-  let selectedDate = '2021-02-18';
-  $("#datepicker").datepicker({
-    dateFormat: "dd/mm/yyyy",
-    inline: true,
-    minDate: "2021-02-18",
-    maxDate: moment(),
-    onSelect: function () {
-      selectedDate = $("#datepicker").val();
+
+  datepickerEl.datepicker({
+    dateFormat: "dd/mm/yy",
+    minDate: new Date("2021-02-18"),
+    maxDate: new Date(),
+    onSelect: function (selectedDate) {
+      console.log(selectedDate);
+      queryImages(new Date(selectedDate).toISOString());
     }
-  });
-
-  console.log(selectedDate);
-
-  $.ajax({
-    url: perseveranceQueryURL(selectedDate),
-    method: "GET",
-  }).then(function (response) {
-    console.log(response.photos);
-
-    let images = response.photos;
-    // Shuffle array
-    const shuffled = images.sort(() => 0.5 - Math.random());
-
-    // Get sub-array of first n elements after shuffled
-    let selected = shuffled.slice(0, 10);
-    console.log(selectedImages);
-    selected.forEach(item => selectedImages.push(item.img_src));
-    console.log(selectedImages);
-
-    selectedImages.forEach(element => {
-      let img = $("<img>").attr('src', element);
-      marsImagesEl.append(img);
-    })
-
-  });
-
-  // titleEl.on("click", function (event) {
-  //   event.preventDefault();
-  //   let marsImagesEl = $("#mars-images");
-  //   let datePickerDateEl = $("#datepicker");
-
-  //   $(".container-fluid").empty();
-  //   marsImagesEl.removeClass('hide');
-
-  //   datePickerDateEl.datepicker({
-  //     onSelect: function(dateText) {
-  //         console.log(dateText);
-  //         dateInput = dateText;
-  //           minDate = 1;
-  //     }
-  //   });
-  // })
-
-  // $.ajax({
-  //     url: perseveranceQueryURL(dateInput),
-  //     method: "GET",
-  //   }).then(function (response) {
-  //     console.log(response.photos);
-  // })
 });
+});
+
 
 function perseveranceQueryURL(date) {
   if (date) {
@@ -83,11 +38,33 @@ function perseveranceQueryURL(date) {
   }
 }
 
-$(function () {
-  $("#datepicker").datepicker();
-  var a = $("#datepicker").datepicker("getDate");
-  console.log(a);
-});
+function queryImages(selectedDate) {
+  $.ajax({
+    url: perseveranceQueryURL(selectedDate),
+    method: "GET",
+  }).then(function (response) {
+    console.log(response.photos);
+    $(".mars-images").removeClass("hide");
+
+    let images = response.photos;
+    // Shuffle array
+    const shuffled = images.sort(() => 0.5 - Math.random());
+    // Get sub-array of first n elements after shuffled
+    let selected = shuffled.slice(0, 10);
+
+    selected.forEach(item => selectedImages.push(item.img_src));
+    console.log(selectedImages);
+    
+    let active = $("<div>").addClass('carousel-item active').append($("<img>").attr('src', selectedImages[0]));
+    $(".carousel-inner").append(active);
+
+    for (let i = 1; i < selectedImages.length; i++) {
+      let imageEl = $("<div>").addClass('carousel-item').append($("<img>").attr('src', selectedImages[i]));
+      $(".carousel-inner").append(imageEl);
+    }
+
+  });
+}
 
 // style datepicker
 // carousel for images
