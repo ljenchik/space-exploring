198d9721f6af7325a3983b8b5d61601e69a3004d
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-02-10 12:00:38 +0000

    Added starteDate to datepicker

--- assets/js/mars-rovers.js
+++ assets/js/mars-rovers.js
@@ -2,30 +2,32 @@ nasaApikey = "zhH0KJERQo5hx19MxD3hGmt6jiaOqgfd8bmoWQPd";
 let imageEl = $("#Perseverance");
 let titleEl = $("#Perseverance-rover");
 let marsImagesEl = $("#mars-images");
-let datepickerEl =  $("#datepicker");
-let datepickerTextEl =  $("#choose-date");
+let datepickerEl = $("#datepicker");
+let datepickerTextEl = $("#choose-date");
+let thumbsList = $("ol");
 let selectedImages = [];
-
 
 imageEl.on("click", function (event) {
   event.preventDefault();
-  
+
   $(".container-fluid").empty();
   datepickerEl.removeClass("hide");
   datepickerTextEl.removeClass("hide");
 
 
+  queryImages("2021-02-18");
+
   datepickerEl.datepicker({
     dateFormat: "dd/mm/yy",
-    minDate: new Date("2021-02-18"),
-    maxDate: new Date(),
+    minDate: "18/02/2021",
+    defaultDate: "18/02/2021",
+    "autoclose": true,
     onSelect: function (selectedDate) {
-      console.log(selectedDate);
-      queryImages(new Date(selectedDate).toISOString());
-    }
+      // Formats date to yyy-mm-dd for api query
+      queryImages(selectedDate.split("/").reverse().join("-"));
+    },
+  });
 });
-});
-
 
 function perseveranceQueryURL(date) {
   if (date) {
@@ -44,7 +46,6 @@ function queryImages(selectedDate) {
     method: "GET",
   }).then(function (response) {
     console.log(response.photos);
-    $(".mars-images").removeClass("hide");
 
     let images = response.photos;
     // Shuffle array
@@ -52,19 +53,57 @@ function queryImages(selectedDate) {
     // Get sub-array of first n elements after shuffled
     let selected = shuffled.slice(0, 10);
 
-    selected.forEach(item => selectedImages.push(item.img_src));
+    selected.forEach((item) => selectedImages.push(item.img_src));
     console.log(selectedImages);
-    
-    let active = $("<div>").addClass('carousel-item active').append($("<img>").attr('src', selectedImages[0]));
+
+    $("#left-arrow").removeClass("hide");
+    $("#right-arrow").removeClass("hide");
+    $("#thumbs-list").removeClass("hide");
+
+    let active = $("<div>")
+      .addClass("carousel-item active")
+      .append($("<img>").attr("src", selectedImages[0]));
     $(".carousel-inner").append(active);
 
+    // First active thumbnail image
+    let activeThumb = $("<li>").addClass("list-inline-item active");
+    let thumbLinkActive = $("<a>")
+      .attr("id", "carousel-selector-0")
+      .addClass("selected")
+      .attr("data-slide-to", "0")
+      .attr("data-target", "#custCarousel");
+    activeThumb.append(thumbLinkActive);
+    thumbLinkActive.append(
+      $("<img>").attr("src", selectedImages[0]).addClass("img-fluid")
+    );
+    thumbsList.append(activeThumb);
+
     for (let i = 1; i < selectedImages.length; i++) {
-      let imageEl = $("<div>").addClass('carousel-item').append($("<img>").attr('src', selectedImages[i]));
+      // Adds images to hero section
+      let imageEl = $("<div>")
+        .addClass("carousel-item")
+        .append($("<img>").attr("src", selectedImages[i]));
       $(".carousel-inner").append(imageEl);
+
+      // Adds images to thumbnail section
+      let thumb = $("<li>").addClass("list-inline-item");
+      let thumbLink = $("<a>")
+        .attr("id", `carousel-selector-${i}`)
+        .attr("data-slide-to", `${i}`)
+        .attr("data-target", "#custCarousel");
+
+      thumb.append(thumbLink);
+      thumbLink.append(
+        $("<img>").attr("src", selectedImages[i]).addClass("img-fluid")
+      );
+      thumbsList.append(thumb);
     }
 
   });
 }
+
+
+
 
 // style datepicker
 // carousel for images
@@ -72,3 +111,6 @@ function queryImages(selectedDate) {
 // media queries for the cards
 // back button
 // work on other cards
+// style carousel
+// chceck dates for datepicker to start with the correct date
+// error message if no images on that day
